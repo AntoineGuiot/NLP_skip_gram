c8a7f827a08ed707ea3efabbe78925f630ebcd56
Antoine Guiot <antoineguiot@MacBook-Pro-de-Antoine-3.local>
2020-02-15 15:45:22 +0100

    sample function

--- skipGram.py
+++ skipGram.py
@@ -1,5 +1,7 @@
 from __future__ import division
 import argparse
+import random
+
 import pandas as pd
 
 # useful stuff
@@ -7,34 +9,43 @@ import numpy as np
 from scipy.special import expit
 from sklearn.preprocessing import normalize
 
+__authors__ = ['author1', 'author2', 'author3']
+__emails__ = ['fatherchristmoas@northpole.dk', 'toothfairy@blackforest.no', 'easterbunny@greenfield.de']
 
-__authors__ = ['author1','author2','author3']
-__emails__  = ['fatherchristmoas@northpole.dk','toothfairy@blackforest.no','easterbunny@greenfield.de']
 
 def text2sentences(path):
-	# feel free to make a better tokenization/pre-processing
-	sentences = []
-	with open(path) as f:
-		for l in f:
-			sentences.append( l.lower().split() )
-	return sentences
+    # feel free to make a better tokenization/pre-processing
+    sentences = []
+    with open(path) as f:
+        for l in f:
+            sentences.append(l.lower().split())
+    return sentences
+
 
 def loadPairs(path):
-	data = pd.read_csv(path, delimiter='\t')
-	pairs = zip(data['word1'],data['word2'],data['similarity'])
-	return pairs
+    data = pd.read_csv(path, delimiter='\t')
+    pairs = zip(data['word1'], data['word2'], data['similarity'])
+    return pairs
 
 
 class SkipGram:
-	def __init__(self, sentences, nEmbed=100, negativeRate=5, winSize = 5, minCount = 5):
-		self.w2id = {} # word to ID mapping
-        self.trainset = {} # set of sentences
-        self.vocab = {} # list of valid words
-        raise NotImplementedError('implement it!')
+    def __init__(self, sentences, nEmbed=100, negativeRate=5, winSize=5, minCount=5):
+        list_words = np.unique(np.concatenate(sentences))
+
+        self.w2id = dict(zip(list_words, np.arange(len(list_words))))
+        self.trainset = sentences  # set of sentences
+        self.winSize = winSize
+        self.vocab = 0  # list of valid words
+        self.negativeRate = negativeRate
 
     def sample(self, omit):
-        """samples negative words, ommitting those in set omit"""
-        raise NotImplementedError('this is easy, might want to do some preprocessing to speed up')
+        omit_id = []
+        for word in omit:
+            omit_id.append(self.w2id[word])
+        w2id_list = list(self.w2id.values())
+        [w2id_list.remove(omit_word_id) for omit_word_id in omit_id]
+        negativeIds = random.choices(w2id_list, self.negativeRate)
+        return negativeIds
 
     def train(self):
         for counter, sentence in enumerate(self.trainset):
@@ -49,55 +60,56 @@ class SkipGram:
                 for context_word in sentence[start:end]:
                     ctxtId = self.w2id[context_word]
                     if ctxtId == wIdx: continue
-                    negativeIds = self.sample({wIdx, ctxtId})
+                    negativeIds = self.sample([wIdx, ctxtId])
                     self.trainWord(wIdx, ctxtId, negativeIds)
                     self.trainWords += 1
 
             if counter % 1000 == 0:
-                print ' > training %d of %d' % (counter, len(self.trainset))
+                print
+                ' > training %d of %d' % (counter, len(self.trainset))
                 self.loss.append(self.accLoss / self.trainWords)
                 self.trainWords = 0
                 self.accLoss = 0.
 
     def trainWord(self, wordId, contextId, negativeIds):
         raise NotImplementedError('here is all the fun!')
+        
+        def save(self, path):
+            raise NotImplementedError('implement it!')
 
-	def save(self,path):
-		raise NotImplementedError('implement it!')
+        def similarity(self, word1, word2):
+            """
+                computes similiarity between the two words. unknown words are mapped to one common vector
+            :param word1:
+            :param word2:
+            :return: a float \in [0,1] indicating the similarity (the higher the more similar)
+            """
+            raise NotImplementedError('implement it!')
 
-	def similarity(self,word1,word2):
-		"""
-			computes similiarity between the two words. unknown words are mapped to one common vector
-		:param word1:
-		:param word2:
-		:return: a float \in [0,1] indicating the similarity (the higher the more similar)
-		"""
-		raise NotImplementedError('implement it!')
+    @staticmethod
+    def load(path):
+        raise NotImplementedError('implement it!')
 
-	@staticmethod
-	def load(path):
-		raise NotImplementedError('implement it!')
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--text', help='path containing training data', required=True)
-	parser.add_argument('--model', help='path to store/read model (when training/testing)', required=True)
-	parser.add_argument('--test', help='enters test mode', action='store_true')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--text', help='path containing training data', required=True)
+    parser.add_argument('--model', help='path to store/read model (when training/testing)', required=True)
+    parser.add_argument('--test', help='enters test mode', action='store_true')
 
-	opts = parser.parse_args()
+    opts = parser.parse_args()
 
-	if not opts.test:
-		sentences = text2sentences(opts.text)
-		sg = SkipGram(sentences)
-		sg.train(...)
-		sg.save(opts.model)
+    if not opts.test:
+        sentences = text2sentences(opts.text)
+        sg = SkipGram(sentences)
+        sg.train(...)
+        sg.save(opts.model)
 
-	else:
-		pairs = loadPairs(opts.text)
+    else:
+        pairs = loadPairs(opts.text)
 
-		sg = SkipGram.load(opts.model)
-		for a,b,_ in pairs:
+        sg = SkipGram.load(opts.model)
+        for a, b, _ in pairs:
             # make sure this does not raise any exception, even if a or b are not in sg.vocab
-			print(sg.similarity(a,b))
-
+            print(sg.similarity(a, b))
