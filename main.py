c0de666df3f296f447165a1c5a6396b079557c7c
Antoine Guiot <you@example.com>
2020-02-23 17:04:36 +0100

    coofsgbrjtn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from skipGram import SkipGram, text2sentences
 
-sentences = text2sentences('train.txt')
+sentences = text2sentences('train_200000.txt')
 print(sentences[0])
 sg = SkipGram(sentences)
 sg.train(2)
